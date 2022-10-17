1604f8de69e98817c1448fbccb390d10ba9c90ba
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-10-17 22:05:05 +0530

    display user list

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,37 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '../../services/database.dart';
 
 class Home extends StatelessWidget {
+  // FirebaseFirestore.instance.collection('users').snapshots();
+  final Stream<QuerySnapshot> collectionReference = Database.readUser();
+
+  // this has to be stateful
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey[800],
         elevation: 0.0,
-        title: Text('Welcome'),
+        title: Text('Welcome Bosa'),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Center(
-          child: Column(
-            children: [
-              Text('Welcome to home page'),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: Text('SignOut'),
-              )
-            ],
-          ),
-        ),
+      body: StreamBuilder(
+        stream: collectionReference,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: ListView(
+                children: snapshot.data!.docs.map((e) {
+                  return Card(
+                      child: Column(children: [
+                    ListTile(
+                      title: Text(e["employee_name"]),
+                      subtitle: Container(
+                        child: (Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Position: " + e['position'],
+                                style: const TextStyle(fontSize: 14)),
+                            Text("Contact Number: " + e['contact_no'],
+                                style: const TextStyle(fontSize: 12)),
+                          ],
+                        )),
+                      ),
+                    ),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.all(5.0),
+                            primary: const Color.fromARGB(255, 143, 133, 226),
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          child: const Text('Edit'),
+                          onPressed: () {
+                            // Navigator.pushAndRemoveUntil<dynamic>(
+                            //   context,
+                            //   MaterialPageRoute<dynamic>(
+                            //     builder: (BuildContext context) => EditPage(
+                            //       employee: Employee(
+                            //           uid: e.id,
+                            //           employeename: e["employee_name"],
+                            //           position: e["position"],
+                            //           contactno: e["contact_no"]),
+                            //     ),
+                            //   ),
+                            //   (route) =>
+                            //       false, //if you want to disable back feature set to false
+                            // );
+                          },
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.all(5.0),
+                            primary: const Color.fromARGB(255, 143, 133, 226),
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          child: const Text('Delete'),
+                          onPressed: () async {
+                            var response =
+                                await Database.deleteEmployee(docId: e.id);
+                            if (response.code != 200) {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      content:
+                                          Text(response.message.toString()),
+                                    );
+                                  });
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ]));
+                }).toList(),
+              ),
+            );
+          }
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Center(
+              child: Column(
+                children: [
+                  Text('Welcome to home page'),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    child: Text('SignOut'),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
+      // body: Container(
+      //   padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+      //   child: Center(
+      //     child: Column(
+      //       children: [
+      //         Text('Welcome to home page'),
+      //         SizedBox(height: 20.0),
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             FirebaseAuth.instance.signOut();
+      //           },
+      //           child: Text('SignOut'),
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
-    // return Center(
-    //   child: Container(
-    //     child: Text('home'),
-    //   ),
-    // );
   }
 }
