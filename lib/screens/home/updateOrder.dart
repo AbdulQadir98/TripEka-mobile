d6a5e6d38b2dc01d4a3f878c8e8ff9254bc2370a
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-11-03 00:16:25 +0530

    normal dropdown

--- lib/screens/home/updateOrder.dart
+++ lib/screens/home/updateOrder.dart
@@ -12,14 +12,24 @@ class _UpdateOrdersState extends State<UpdateOrders> {
   final List<int> strengths = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
   // form values
-  String? _currentName;
-  String? _currentSugars;
-  int? _currentStrength;
+  String _currentName = "Vili";
+  String _currentSugars = "20";
+  int _currentStrength = 500;
 
   // hardcode values
   String _name = "Abdul";
   String _sugars = "10";
   int _strength = 300;
+
+  // Example normal dropdown
+  String dropdownvalue = 'Item 1';
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,21 +43,41 @@ class _UpdateOrdersState extends State<UpdateOrders> {
           SizedBox(height: 20.0),
           TextFormField(initialValue: _name),
           SizedBox(height: 10.0),
-          DropdownButtonFormField(
-            value: _currentSugars ?? _sugars,
-            items: sugars.map((sugar) {
+          // DropdownButtonFormField(
+          //   value: _currentSugars,
+          //   items: sugars.map((sugar) {
+          //     return DropdownMenuItem(
+          //       value: sugar,
+          //       child: Text('$sugar sugars'),
+          //     );
+          //   }).toList(),
+          //   onChanged: (val) => setState(() => _currentSugars = val),
+          // ),
+          DropdownButton(
+            // Initial Value
+            value: dropdownvalue,
+            // Down Arrow Icon
+            icon: const Icon(Icons.keyboard_arrow_down),
+            // Array list of items
+            items: items.map((String items) {
               return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
+                value: items,
+                child: Text(items),
               );
             }).toList(),
-            onChanged: (val) => setState(() => _currentSugars = "4"),
+            // After selecting the desired option,it will
+            // change button value to selected value
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownvalue = newValue!;
+              });
+            },
           ),
           SizedBox(height: 10.0),
           Slider(
-            value: (_currentStrength ?? _strength).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? _strength],
-            inactiveColor: Colors.brown[_currentStrength ?? _strength],
+            value: (_currentStrength).toDouble(),
+            activeColor: Colors.brown[_currentStrength],
+            inactiveColor: Colors.brown[_currentStrength],
             min: 100.0,
             max: 900.0,
             divisions: 8,
